4a188334a0cef431886c8ce33e170acddb773a86
herzfeld <bulddima@gmail.com>
2021-08-24 21:45:16 +0300

    Initial setup
    Node packages, object models, routes and controllers, cloud database

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://herzfeld:261997ka@crm-mean-cluster.qp6z1.mongodb.net/' +
+    'myFirstDatabase?retryWrites=true&writeConcern=majority',
+  mongoSetting: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+}
